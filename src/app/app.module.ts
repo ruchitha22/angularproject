c68aea67b07fb7ecf28a20ba4c9640b4f8a54b88
Ruchitha Kondreddy <ruchitha.110022@gmail.com>
2020-08-30 12:19:34 +0530

    create login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { Routes } from '@angular/router';
+const routes: Routes = [
+{
+  'path': 'login', component: LoginComponent
+},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
